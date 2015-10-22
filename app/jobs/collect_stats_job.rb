4c4f1a19bfe37e672667e76f0086ea8b5231e027
Liam Norton <liam.norton@flippa.com>
2015-10-22 21:21:49 +1100

    Add CollectStatsJob

--- app/jobs/collect_stats_job.rb
+++ app/jobs/collect_stats_job.rb
@@ -4,12 +4,12 @@ class CollectStatsJob < ActiveJob::Base
   def perform(*args)
     Game.include_players.running.each do |game|
       players = [
-	game.home_team.players.all,
-	game.away_team.players.all
+        game.home_team.players.all,
+        game.away_team.players.all
       ].flatten
 
       players.each do |player|
-	Draft::Stats.new(game: game, player: player).collect!
+        Draft::Stats.new(game: game, player: player).collect!
       end
     end
   end
--- spec/jobs/collect_stats_job_spec.rb
+++ spec/jobs/collect_stats_job_spec.rb
@@ -1,16 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe CollectStatsJob, type: :job do
-  it "calls for game stats to be collected for players" do
-    game = create(:running_game)
+  it "calls for game stats to be collected for running games" do
+    unstarted_game = create(:game)
+    running_game = create(:running_game)
+    completed_game = create(:completed_game)
 
-    player = create(:player, team: game.home_team)
+    inactive_player = create(:player, team: unstarted_game.home_team)
+    active_player = create(:player, team: running_game.home_team)
+    old_player = create(:player, team: completed_game.home_team)
 
     stats_collector = instance_double(Draft::Stats)
 
     allow(Draft::Stats).
       to receive(:new).
-      with(game: game, player: player).
+      with(game: running_game, player: active_player).
       and_return(stats_collector)
 
     expect(stats_collector).to receive(:collect!)
