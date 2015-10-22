775929f7ac43a9501f622e81de23758e50677b33
Liam Norton <liam.norton@flippa.com>
2015-10-22 20:47:59 +1100

    Spec contest scoring

--- spec/models/roster_spec.rb
+++ spec/models/roster_spec.rb
@@ -22,4 +22,37 @@ RSpec.describe Roster, type: :model do
       end
     end
   end
+
+  describe "#score" do
+    it "returns sum of stat points" do
+      contest = create(:contest)
+      game = create(:game, round: contest.round)
+
+      player_1 = create(:player, team: game.home_team)
+      player_2 = create(:player, team: game.away_team)
+
+      create(:stat, player: player_1, game: game, points: 100)
+      create(:stat, player: player_2, game: game, points: 100)
+
+      roster = create(:roster, contest: contest)
+      create(:draft_pick, roster: roster, player: player_1)
+
+      expect(roster.score).to eq(100)
+    end
+
+    it "returns 0 with no stats" do
+      contest = create(:contest)
+      game = create(:game, round: contest.round)
+
+      player_1 = create(:player, team: game.home_team)
+      player_2 = create(:player, team: game.away_team)
+
+      create(:stat, player: player_1, game: game, points: 100)
+      create(:stat, player: player_2, game: game, points: 100)
+
+      roster = create(:roster, contest: contest)
+
+      expect(roster.score).to eq(0)
+    end
+  end
 end
